fac6c9378b0530a324b894062cbcb8495385af06
Vuyisile Tlotliso Lehola <73833164+vtl-28@users.noreply.github.com>
2025-03-15 08:09:35 +0000

    add new location property to model

--- Controllers/TodosController.cs
+++ Controllers/TodosController.cs
@@ -88,7 +88,7 @@ namespace DotNetCoreSqlDb.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Description,CreatedDate")] Todo todo)
+        public async Task<IActionResult> Create([Bind("ID,Description,Location,CreatedDate")] Todo todo)
         {
             if (ModelState.IsValid)
             {
@@ -143,7 +143,7 @@ namespace DotNetCoreSqlDb.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Description,CreatedDate")] Todo todo)
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Description,Location,CreatedDate")] Todo todo)
         {
             if (id != todo.ID)
             {
---
+++ Migrations/20250315080246_AddLocationProperty.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetCoreSqlDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLocationProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Todo",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Todo");
+        }
+    }
+}

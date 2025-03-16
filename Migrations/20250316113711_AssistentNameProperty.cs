44f0699ddf11522fa54866b928909950dbab6d2b
Vuyisile Tlotliso Lehola <73833164+vtl-28@users.noreply.github.com>
2025-03-16 11:46:26 +0000

    add assitentnae property

---
+++ Migrations/20250316113711_AssistentNameProperty.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetCoreSqlDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AssistentNameProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AssistentName",
+                table: "Todo",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssistentName",
+                table: "Todo");
+        }
+    }
+}

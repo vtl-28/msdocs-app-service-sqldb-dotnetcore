3c10925956731167dd01982fc1efa2a8ff823bbc
Vuyisile Tlotliso Lehola <73833164+vtl-28@users.noreply.github.com>
2025-03-16 08:20:46 +0000

    Rename location property and make changes to the affect view and controller files

---
+++ Migrations/20250316081907_RenameLocationProperty.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetCoreSqlDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameLocationProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Location",
+                table: "Todo",
+                newName: "Locations");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Locations",
+                table: "Todo",
+                newName: "Location");
+        }
+    }
+}
--- Models/Todo.cs
+++ Models/Todo.cs
@@ -7,7 +7,7 @@ namespace DotNetCoreSqlDb.Models
     {
         public int ID { get; set; }
         public string? Description { get; set; }
-        public string? Location { get; set; }
+        public string? Locations { get; set; }
 
         [DisplayName("Created Date")]
         [DataType(DataType.Date)]
